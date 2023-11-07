9fdc9b11bc5980a5f687cc1d0a9f3c59c1c5bfa3
KamauDev-maker <oscarnjenga@gmail.com>
2023-11-07 11:45:36 -0500

    creation of a new route

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -5,6 +5,14 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """
+    creation of a new route
+    """
+    abort(401)
+
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
