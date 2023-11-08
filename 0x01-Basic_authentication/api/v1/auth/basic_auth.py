bdf5bc9c32a1514e406408ef6fccc8f78a243cce
KamauDev-maker <oscarnjenga@gmail.com>
2023-11-08 10:06:36 -0500

    basic base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 Basic auth
 """
 import re
+import base64
+import binascii
 from .auth import Auth
 from models.user import User
 
@@ -22,3 +24,19 @@ class BasicAuth(Auth):
             if field_match is not None:
                 return field_match.group('token')
         return None
+
+
+def decode_base64_authorization_header(
+    self,
+    base64_authorization_header: str
+) -> str:
+    """"a method that decodes value base64string"""
+    if type(base64_authorization_header) == str:
+        try:
+            result = base64.b64decode(
+                extract_base64_authorization_header,
+                validate=True,
+            )
+            return result.decode('utf-8')
+        except (binascii.Error, UnicodeDecodeError):
+            return None
