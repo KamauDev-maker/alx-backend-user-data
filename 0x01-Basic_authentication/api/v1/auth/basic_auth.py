b9dac42ad279025fe3aad1f217e1355297f9e557
KamauDev-maker <oscarnjenga@gmail.com>
2023-11-08 10:13:39 -0500

    basic user credential

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,6 +7,7 @@ import base64
 import binascii
 from .auth import Auth
 from models.user import User
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -40,3 +41,21 @@ def decode_base64_authorization_header(
             return result.decode('utf-8')
         except (binascii.Error, UnicodeDecodeError):
             return None
+
+
+def extract_user_credentials(
+    self,
+    decoded_base64_authorization_header: str
+) -> Tuple[str, str]:
+    """"extracts user credentials"""
+    if type(decoded_base64_authorization_header) == str:
+        pattern = r'(?P<user>[^:]+):(?P<password>.+)'
+        field_match = re.fullmatch(
+            pattern,
+            decoded_base64_authorization_header.strip(),
+        )
+        if field_match is not None:
+            user = field_match.group('user')
+            password = field_match.group('password')
+            return user, password
+        return None, None
