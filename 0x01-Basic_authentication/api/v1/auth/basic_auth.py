c4b8ef04bcb15080cea92b1c1479ddaa52dd023d
KamauDev-maker <oscarnjenga@gmail.com>
2023-11-08 09:56:33 -0500

    basic -basic4 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+"""
+Basic auth
+"""
+import re
+from .auth import Auth
+from models.user import User
+
+
+class BasicAuth(Auth):
+    """
+    basic auth class
+    """
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str
+    ) -> str:
+        """Extracts the authorization header"""
+        if type(authorization_header) == str:
+            pattern = r'Basic(?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+        return None
