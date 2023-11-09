8d9b3e396f89cdf24189f222750462d1a42c7199
KamauDev-maker <oscarnjenga@gmail.com>
2023-11-09 10:26:31 -0500

    before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -69,10 +69,14 @@ def handle_request():
         '/api/v1/status/',
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/',
     ]
     if not auth.require_auth(request.path, excluded_paths):
         return
     auth_header = auth.authorization_header(request)
+    session_cookie = auth.session_cookie(request)
+    if auth_header is None and session_cookie is None:
+        abort(401)
     user = auth.current_user(request)
     if user is None:
         abort(403)
