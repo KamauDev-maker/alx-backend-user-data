bd33f85339973a7b6b75435d547eedd2094cf1c3
KamauDev-maker <oscarnjenga@gmail.com>
2023-11-15 16:49:46 +0000

    find a user in db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import User, Base
 
@@ -38,3 +40,16 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs):
+        """
+        find a user in the DB
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound("No user found with given name")
+            return user
+        except InvalidRequestError as e:
+            self._session.rollback()
+            raise e
